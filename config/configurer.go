956cbe4db0344cc30d926f6f88317136565116cd
Cyril Dumange <cyril.dumange@zenika.com>
2024-07-22 10:13:50 -0400

    feat: basic empty configurer

--- config/configurer.go
+++ config/configurer.go
@@ -11,25 +11,23 @@ type cSharpConfigurer struct{}
 
 // CheckFlags implements config.Configurer.
 func (cSharpConfigurer) CheckFlags(fs *flag.FlagSet, c *config.Config) error {
-	panic("unimplemented")
+	return nil
 }
 
 // Configure implements config.Configurer.
 func (cSharpConfigurer) Configure(c *config.Config, rel string, f *rule.File) {
-	panic("unimplemented")
 }
 
 // KnownDirectives implements config.Configurer.
 func (c cSharpConfigurer) KnownDirectives() []string {
-	panic("unimplemented")
+	return nil
 }
 
 // RegisterFlags implements config.Configurer.
 func (cSharpConfigurer) RegisterFlags(fs *flag.FlagSet, cmd string, c *config.Config) {
-	panic("unimplemented")
 }
 
-// NewConfigurer returns the configurer for csharp language
+// NewConfigurer returns the configurer for csharp language.
 func NewConfigurer() config.Configurer {
 	return cSharpConfigurer{}
 }
